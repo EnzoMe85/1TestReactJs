62edb8c8c7c73f284de236ffca102078b031d8f5
EnzoMe85 <112831815+EnzoMe85@users.noreply.github.com>
2022-11-23 17:18:38 +0100

    Add files via upload

---
+++ src/Product.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const prezzo = '14,00';
+const alt = 'valore';
+const img = "https://egsiteassets.images.egifter.com/Images/GiftCardFaceplates/External/ADIDAS_fp01.png";
+
+const Product = () => {
+    return (
+        <article>
+            <div className='articolo'>
+                <img className='imageCard' src={img} alt={`${alt}`} />
+                <h6>10€ Amazon + 20P</h6>
+                <hr />
+                <p>2,16 €</p>
+                <p>bamby000</p>
+                <p>RIAPRE PRESTO</p>
+                <a href="http://Adidas.com">
+                <button className='myButton'>
+                    {prezzo} €
+                </button>
+                </a>
+            </div>
+        </article >
+    )
+}
+
+export default Product
